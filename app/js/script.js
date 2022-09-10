33ef408cf1a89116ba2cb47b2a5886df15124cb3
badukwei <linwg861111@gmail.com>
2022-09-10 20:58:17 +0800

    Create animation and ui
    buttons, add items, remove items, count price, fade-in and out animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,57 +1,142 @@
-//To do : Let the input boxes sizing with the text and set the placeholder
+//To do : animation of removing and price change
+//Bug : if I don't put the item name price will cover the remove bottun
+//To fix : 自動進位
+//To fix : defaults of input
 
-//Delete button
-let deleteButton = document.querySelectorAll(".delete");
-
-let deleteTask = function() {
-    console.log("Delete task...");
-    let listItem = this.parentNode.parentNode.parentNode;
-    let item = listItem.parentNode;  
-    item.removeChild(listItem);
-}
-//use forEach on the class or use Event delegation
-deleteButton.forEach( element => element.addEventListener('click', deleteTask));
-
-//Add the price
-let priceOutput = document.querySelectorAll(".priceOutput"); 
-let itemPrice = document.querySelectorAll(".price");
-let itemAmount = document.querySelectorAll(".amount");
-
-let addThePrices = function() {
-    console.log("has added");
-    //To do:Turn it into functional 
-    this.parentNode.childNodes[4].innerHTML = this.value * this.parentNode.childNodes[2].value;
-}
- 
-itemAmount.forEach( element => element.addEventListener('change', addThePrices));
-//itemPrice.forEach( element => element.addEventListener('change', addThePrices));
-
+let idInput = document.getElementById("new-id");
+let nameInput = document.getElementById("new-name");
+let amountInput = document.getElementById("new-amount");
+let priceInput = document.getElementById("new-price");
+let addButton = document.getElementsByTagName("button")[0];
+let itemsHolder = document.getElementById("itemWrap");
 //Total Price and Tips
 let subTotal = document.getElementById('subTotal');
 let tips = document.getElementById('tips');
 let total = document.getElementById('total');
 
+const createNewItemElement = function(number, name, amout, price) {
+  //create a listitem
+  let item = document.createElement("div");
+  //itemNumber & itemName & itemPrice & itemAmount
+  let infoWrap = document.createElement("div"); 
+  //itemNumber & itemName
+  let info = document.createElement("div");
+  //itemNumber
+  let itemNumber = document.createElement("p"); 
+  //itemName
+  let itemName = document.createElement("h1");
+  //total-price
+  let totalPrice = document.createElement("p");
+  //priceOutput
+  let priceOutput = document.createElement("span");
+  //removeWrap
+  let removeWrap = document.createElement("div");
+  //deleteButton
+  let deleteButton = document.createElement("label");
+
+  //classname
+  item.className="item"
+  infoWrap.className = "infoWrap";
+  info.className = "info";
+  itemNumber.className = "itemNumber";
+  itemName.className = "itemName";
+  totalPrice.className = "total-price";
+  priceOutput.className = "priceOutput";
+  removeWrap.className = "removeWrap";
+  deleteButton.classList.add("delete", "click-button");
+ 
+
+  //innertext
+  itemNumber.innerText = number;
+  itemName.innerText = name;
+  priceOutput.innerText = parseFloat(amout) * parseFloat(price);
+  totalPrice.innerText = "$";
+  deleteButton.innerText = "x";
+
+  //apendchild
+  item.appendChild(infoWrap);
+  infoWrap.appendChild(info);
+  infoWrap.appendChild(totalPrice);
+  infoWrap.appendChild(removeWrap);
+  info.appendChild(itemNumber);
+  info.appendChild(itemName);
+  totalPrice.appendChild(priceOutput);
+  removeWrap.appendChild(deleteButton);
+
+  console.log(item);
+  return item;
+}
+
+//add items
+const addItem = function() {
+  console.log("Add task...");
+  //Create a new list item with the text
+  let listItem = createNewItemElement(idInput.value, nameInput.value, amountInput.value, priceInput.value);
+  //Append listItem to itemsHolder
+  itemsHolder.appendChild(listItem); 
+  bindItemEvents(listItem);
+  listItem.classList.add("fade-in");
+  //subTotal.classList.add("fade-in");
+  //tips.classList.add("fade-in");
+  //total.classList.add("fade-in");
+  setTimeout(() => {
+    countPrice();
+  }, 500);
+ 
+  //defaults?
+  idInput.value = "";   
+  nameInput.value = "";
+  amountInput.value = "";
+  priceInput.value = "";
+}
+addButton.addEventListener("click", addItem);
+
+//delete items
+let deleteItem = function() {
+    console.log("Delete task...");
+    let listItem = this.parentNode.parentNode.parentNode;
+    //subTotal.classList.add("fade-in");
+    //tips.classList.add("fade-in");
+    //total.classList.add("fade-in"); 
+    listItem.classList.add("fade-out"); 
+    setTimeout(() => {
+      listItem.remove();
+      countPrice();
+    }, 500);     
+}
+
+let bindItemEvents = function(listItem) {
+  console.log("Bind list item events");
+  //select listItem's children
+  let deleteButton = listItem.querySelector(".delete");
+  let price = listItem.querySelector(".priceOutput").innerText;
+  console.log(price);
+  //bind deleteItem to delete button
+  deleteButton.onclick = deleteItem;
+}
+
+for(let i = 0; i < itemsHolder.children.length; i++) {
+  //bind events to list item's children
+  bindItemEvents(itemsHolder.children[i]);
+}
+
 //Sum the price
 let countPrice = function() {
-    console.log('Counting the price');
-    subTotal.innerHTML = 0;
-    for(let i = 0; i < priceOutput.length; i++) {
-        subTotal.innerHTML = parseFloat(priceOutput[i].innerHTML) + parseFloat(subTotal.innerHTML)
-    }
+  console.log('Counting the price');
+  subTotal.innerHTML = 0;
+  let priceOutputSum = itemsHolder.querySelectorAll('.priceOutput');
+  for(let i = 0; i < priceOutputSum.length; i++) {
+      subTotal.innerHTML = (parseFloat(priceOutputSum[i].innerHTML) + parseFloat(subTotal.innerHTML)).toFixed(2)
+  }
+  tips.innerHTML = (parseFloat(subTotal.innerHTML) * 0.1).toFixed(2);
+  total.innerHTML = (parseFloat(subTotal.innerHTML) + parseFloat(tips.innerHTML)).toFixed(2);
+  subTotal.classList.add("fade-in");
+  tips.classList.add("fade-in");
+  total.classList.add("fade-in");
+  setTimeout(function(){
+    subTotal.classList.remove('fade-in');
+    tips.classList.remove("fade-in");
+    total.classList.remove("fade-in");
+  }, 700)
 } 
-subTotal.addEventListener('click', countPrice);
-
-//Count tips 
-let countTips = function() {
-    tips.innerHTML = parseFloat(subTotal.innerHTML) * 0.1
-} 
-tips.addEventListener('click', countTips);
-
-//Count total
-let countTotal = function() {
-    total.innerHTML = parseFloat(subTotal.innerHTML) +  parseFloat(tips.innerHTML)
-    console.log(total.innerHTML)
-} 
-total.addEventListener('click', countTotal);
-
-//Bug: total still count the removes 
+countPrice();
